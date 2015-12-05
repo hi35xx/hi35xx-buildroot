709d80dd2c8460d68ae3dcdcc5032ca93b2bca8d
Watson Xu <xuhuashan@gmail.com>
2015-12-05 21:14:48 +0800

    archivemount: add dependencies to host-pkgconf

--- package/archivemount/archivemount.mk
+++ package/archivemount/archivemount.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 ARCHIVEMOUNT_VERSION = 0.8.5
-ARCHIVEMOUNT_SITE = http://www.cybernoia.de/software/archivemount/
-ARCHIVEMOUNT_DEPENDENCIES = libfuse libarchive
+ARCHIVEMOUNT_SITE = http://www.cybernoia.de/software/archivemount
+ARCHIVEMOUNT_DEPENDENCIES = host-pkgconf libfuse libarchive
 ARCHIVEMOUNT_LICENSE = LGPL
 ARCHIVEMOUNT_LICENSE_FILES = COPYING
 
