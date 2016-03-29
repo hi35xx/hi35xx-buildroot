05ecbc4ef5d14ea38b6508e6d93839fb24e9a94d
Watson Xu <xuhuashan@gmail.com>
2016-03-29 15:43:54 +0800

    rtl8188eu: use realtek official v4.3.0.8 based driver instead.
    This driver supports more features like AP mode, P2P, etc.

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-RTL8188EU_VERSION = 3091828c8f4b4a01cbec6025128bf77e6e7b9f97
-RTL8188EU_SITE = $(call github,lwfinger,rtl8188eu,$(RTL8188EU_VERSION))
-RTL8188EU_LICENSE = GPLv2, proprietary (rtl8188eufw.bin firmware blob)
+RTL8188EU_VERSION = af12a3a198546c0e558464f75e0d36808f2c1851
+RTL8188EU_SITE = $(call github,hi35xx,rtl8188eu,$(RTL8188EU_VERSION))
+RTL8188EU_LICENSE = GPLv2
 RTL8188EU_LICENSE_FILES = COPYING
 RTL8188EU_MODULE_MAKE_OPTS = CONFIG_RTL8188EU=m
+RTL8188EU_MODULE_MAKE_OPTS += "USER_EXTRA_CFLAGS = -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT -DCONFIG_P2P_IPS"
 
 $(eval $(kernel-module))
-
-define RTL8188EU_INSTALL_FIRMWARE
-	$(INSTALL) -D -m 644 $(@D)/rtl8188eufw.bin \
-		$(TARGET_DIR)/lib/firmware/rtlwifi/rtl8188eufw.bin
-endef
-RTL8188EU_POST_INSTALL_TARGET_HOOKS += RTL8188EU_INSTALL_FIRMWARE
-
 $(eval $(generic-package))
