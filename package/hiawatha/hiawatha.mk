a96dd1d35c0b6cff9b95fb665f760ff9db27e9bf
Watson Xu <xuhuashan@gmail.com>
2017-03-19 20:41:33 +0800

    hiawatha: add init script, change work and log directory

--- package/hiawatha/hiawatha.mk
+++ package/hiawatha/hiawatha.mk
@@ -18,12 +18,32 @@ HIAWATHA_CONF_OPTS += -DENABLE_TLS=OFF
 endif
 
 HIAWATHA_CONF_OPTS += \
-	-DENABLE_TOOLKIT=OFF \
+	-DENABLE_TOOLKIT=ON \
 	-DENABLE_XSLT=OFF \
 	-DCONFIG_DIR=/etc/hiawatha \
-	-DLOG_DIR=/var/log \
+	-DLOG_DIR=/var/log/hiawatha \
 	-DPID_DIR=/var/run \
 	-DWEBROOT_DIR=/var/www/hiawatha \
 	-DWORK_DIR=/var/lib/hiawatha
+
+# fix rebuild (hiawatha makefile errors out if /var/lib/hiawatha is a symlink)
+define HIAWATHA_REMOVE_VAR_LIB_HIAWATHA
+	rm -rf $(TARGET_DIR)/var/lib/hiawatha
+endef
+
+HIAWATHA_POST_BUILD_HOOKS += HIAWATHA_REMOVE_VAR_LIB_HIAWATHA
+
+define HIAWATHA_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/hiawatha/S50hiawatha \
+		$(TARGET_DIR)/etc/init.d/S50hiawatha
+
+	mkdir -p $(TARGET_DIR)/var/lib
+	rm -rf $(TARGET_DIR)/var/lib/hiawatha
+	ln -sf /tmp/hiawatha $(TARGET_DIR)/var/lib/hiawatha
+endef
+
+define HIAWATHA_INSTALL_INIT_SYSTEMD
+	mkdir -p $(TARGET_DIR)/var/lib/hiawatha
+endef
 
 $(eval $(cmake-package))
