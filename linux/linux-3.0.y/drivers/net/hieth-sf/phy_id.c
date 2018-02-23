cc3f68923195d0692e1bdadf3c31a16c5215211c
Igor Zalatov <zig@dolphin.flymon.net>
2018-02-23 16:08:25 +0300

    Enable support hiphy_driver_SMSC_a

--- linux/linux-3.0.y/drivers/net/hieth-sf/phy_id.c
+++ linux/linux-3.0.y/drivers/net/hieth-sf/phy_id.c
@@ -14,7 +14,6 @@ struct hiphy_info *phy_search_ids(unsigned long phy_id)
 	return fit_info;
 }
 
-
 #if RTL8211EG_MAC
 static struct hiphy_driver  hiphy_driver_rtl8211EG_mac = {
 	.eee_enable = phy_realtek_b_mac_eee_enable,
@@ -29,8 +28,13 @@ static struct hiphy_driver  hiphy_driver_smsc_lan8740 = {
 	.eee_enable = phy_smsc_a_eee_enable,
 };
 
+static struct hiphy_driver  hiphy_driver_SMSC_a = {
+	.eee_enable = phy_smsc_a_eee_enable,
+};
+
 struct hiphy_info phy_info_table[] = {
 	/* phy_name		phy_id	  with_eee phy_driver */
+
 /* SMSC */
 	{"SMSC LAN8740",	0x7c110,  MAC_EEE, &hiphy_driver_smsc_lan8740},
 
@@ -40,10 +44,10 @@ struct hiphy_info phy_info_table[] = {
 	{"Realtek 8211EG",	0x1cc915, PHY_EEE, &hiphy_driver_rtl8211EG},
 #endif
 
-/*	{"Realtek 8201",	0x1cc816,  MAC_EEE, &hiphy_driver_SMSC_a},
-
+	{"Realtek 8201",	0x1cc816,  MAC_EEE, &hiphy_driver_SMSC_a},
 	{"IC+ IP101G_DS_R01",	0x2430c54,  MAC_EEE, &hiphy_driver_SMSC_a},
-	{"Micrel KSZ8091MNX-RNB",   0x220000,  MAC_EEE, &hiphy_driver_SMSC_a},*/
+	{"Micrel KSZ8091MNX-RNB",   0x220000,  MAC_EEE, &hiphy_driver_SMSC_a},
+
 	{0, 0, 0, 0},
 };
 
