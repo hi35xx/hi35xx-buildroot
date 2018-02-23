a823a59c1e67b4ecc3b558b8199f1e8680bb2d08
Igor Zalatov <zig@dolphin.flymon.net>
2018-02-23 16:02:39 +0300

    Disable some features for vtun

--- package/vtun/vtun.mk
+++ package/vtun/vtun.mk
@@ -8,13 +8,13 @@ VTUN_VERSION = 3.0.3
 VTUN_SITE = http://downloads.sourceforge.net/project/vtun/vtun/$(VTUN_VERSION)
 VTUN_LICENSE = GPLv2+ with OpenSSL exception
 VTUN_LICENSE_FILES = README.OpenSSL
-VTUN_DEPENDENCIES = host-bison host-flex zlib lzo openssl
+VTUN_DEPENDENCIES = host-bison host-flex
 VTUN_AUTORECONF = YES
 
 VTUN_CONF_OPTS = \
-	--with-ssl-headers=$(STAGING_DIR)/usr/include/openssl \
-	--with-lzo-headers=$(STAGING_DIR)/usr/include/lzo \
-	--with-lzo-lib=$(STAGING_DIR)/usr/lib
+	--disable-lzo \
+	--disable-zlib \
+	--disable-ssl
 
 # Assumes old-style gcc inline symbol visibility rules
 VTUN_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu89"
